567d231cc3b4857616948ff1288b22be24634870
Nikita Verbovikov <nikitaverbovikov@gmail.com>
2024-01-04 23:53:30 +0300

    feat: add -date flag

--- main.go
+++ main.go
@@ -10,10 +10,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 const (
-	csvHeader     = "IP Address"
 	fieldsCountry = "country"
 	url           = "http://ip-api.com/batch"
 )
@@ -29,6 +30,7 @@ type ipToCheck struct {
 
 func main() {
 	filePath := flag.String("file", "access_logs.csv", "Name of the CSV file with Slack logs")
+	dateFrom := flag.String("date", "", "Date from which until today you are interested in records (DD-MM-YYYY")
 	flag.Parse()
 
 	records, err := readCSVFile(*filePath)
@@ -36,7 +38,12 @@ func main() {
 		log.Fatalf("failed to read records from CSV: %s", err.Error())
 	}
 
-	uniqIPs := sortUniqIPs(records)
+	records, err = filterByDate(records, *dateFrom)
+	if err != nil {
+		log.Fatalf("failed to sort records by date: %s", err)
+	}
+
+	uniqIPs := getUniqIPs(records)
 	payload, err := prepareRequestPayload(uniqIPs)
 	if err != nil {
 		log.Fatalf("failed to prepare request payload: %s", err.Error())
@@ -66,13 +73,33 @@ func readCSVFile(filePath string) ([][]string, error) {
 	return records, nil
 }
 
-func sortUniqIPs(records [][]string) (IPs []string) {
-	unique := make(map[string]struct{})
-	for i := range records {
-		unique[records[i][3]] = struct{}{}
+func filterByDate(records [][]string, dateFrom string) ([][]string, error) {
+	parsedDateFrom, err := time.Parse("02-01-2006", dateFrom)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse the date from which records will be filtered: %w", err)
 	}
 
-	delete(unique, csvHeader)
+	i := 1
+	for ; i < len(records)-1; i++ {
+		records[i][0], _, _ = strings.Cut(records[i][0], "GMT")
+		t, err := time.Parse("Mon Jan 02 2006 15:04:05", strings.TrimSpace(records[i][0]))
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse the time: %w", err)
+		}
+
+		if t.Before(parsedDateFrom) {
+			break
+		}
+	}
+
+	return records[:i], nil
+}
+
+func getUniqIPs(records [][]string) (IPs []string) {
+	unique := make(map[string]struct{})
+	for i := 1; i < len(records)-1; i++ {
+		unique[records[i][3]] = struct{}{}
+	}
 
 	for key := range unique {
 		IPs = append(IPs, key)
